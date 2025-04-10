9d90822f3a3a4bb7a578b3e51c9476a60bb2ac62
Richard Alhama <richard.alhama@gmail.com>
2025-04-10 20:14:06 +0800

    commented register link

--- permit-app/src/components/NavbarComponent.jsx
+++ permit-app/src/components/NavbarComponent.jsx
@@ -5,10 +5,10 @@ import { Navbar, Nav } from 'react-bootstrap'
 const NavbarComponent = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Permit App</Navbar.Brand>
+      <Navbar.Brand href="/">Sp. Permits Management</Navbar.Brand>
       <Nav className="ml-auto">
         <Link className="nav-link" to="/login">Login</Link>
-        <Link className="nav-link" to="/register">Register</Link>
+        {/* <Link className="nav-link" to="/register">Register</Link> */}
         <Link className="nav-link" to="/permits">Permits</Link>
       </Nav>
     </Navbar>
