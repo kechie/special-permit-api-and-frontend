5d726d43dc67e86b88f41e9373bd0c892fff0d00
Richard Alhama <richard.alhama@gmail.com>
2025-04-14 15:01:12 +0800

    added some tricks my babe klebsiella

--- permit-app/src/utils/helpers.js
+++ permit-app/src/utils/helpers.js
@@ -1,3 +1,4 @@
+// Helper functions for the application
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 };
@@ -5,3 +6,12 @@ export function capitalizeFirstLetter(string) {
 export function formatDate(date) {
   return date ? new Date(date).toLocaleDateString() : 'N/A';
 };
+
+//function capitalizeFirstLetter(string) {
+//  return string.replace(/^./, string[0].toUpperCase())
+//}
+
+// function capitalizeFirstLetter(string) {
+//   return string.split('').map((char, index) =>
+//     index === 0 ? char.toUpperCase() : char).join('')
+// }
