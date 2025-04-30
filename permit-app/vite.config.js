217f50da00664d16864722646c50959e36db645b
Richard Alhama <richard.alhama@gmail.com>
2025-04-30 08:19:39 +0800

    added build options to suppres warnigs

--- permit-app/vite.config.js
+++ permit-app/vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spclpermits/', // Match the subdirectory
+  base: '/spclpermits/',
+  build: {
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            'react-bootstrap',
+            'axios'
+          ],
+          charts: ['xlsx'],
+          icons: ['react-bootstrap-icons']
+        }
+      }
+    },
+    sourcemap: false,
+    //minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 });
